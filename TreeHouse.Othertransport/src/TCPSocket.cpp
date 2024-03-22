f46a0add3628bd663e4e915cf96a9616d143f09e
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2024-03-22 20:51:34 +0100

    Refactored TCPSocket to use coroutines

--- TreeHouse.Othertransport/src/TCPSocket.cpp
+++ TreeHouse.Othertransport/src/TCPSocket.cpp
@@ -5,118 +5,97 @@
 
 void TCPSocket::connect(uint32_t ip, uint16_t port)
 {
-	//TODO: Make this timeout a little sooner.
+	asio::co_spawn(strand, connectAsync(ip, port), asio::detached);
+}
+
+asio::awaitable<void> TCPSocket::connectAsync(uint32_t ip, uint16_t port)
+{
 	asio::ip::tcp::endpoint endpoint(asio::ip::address_v4(_byteswap_ulong(ip)), port);
-	socket.async_connect(endpoint, asio::bind_executor(strand, [this](asio::error_code error)
+	
+	//TODO: Make this timeout a little sooner.
+	try
 	{
-		Buffer buffer(1);
+		co_await socket.async_connect(endpoint, asio::use_awaitable);
+	}
+	catch (const std::system_error& e)
+	{
+		LOG_DEBUG(L"Connect failed! %S", e.what());
+		Buffer responseBuffer(1);
+		responseBuffer.data()[0] = OLDefaultMessageIDTypes::OL_ID_CONNECTION_ATTEMPT_FAILED;
+		inbox.enqueue(std::move(responseBuffer));
+	}
 
-		if (error)
-		{
-			LOG_DEBUG(L"Connect failed! %S", error.message().c_str());
-			buffer.data[0] = OLDefaultMessageIDTypes::OL_ID_CONNECTION_ATTEMPT_FAILED;
-		}
-		else
-		{
-			LOG_DEBUG(L"Connect success!");
-			buffer.data[0] = OLDefaultMessageIDTypes::OL_ID_CONNECTION_REQUEST_ACCEPTED;
-			doRead();
-		}
+	LOG_DEBUG(L"Connect success!");
+	Buffer responseBuffer(1);
+	responseBuffer.data()[0] = OLDefaultMessageIDTypes::OL_ID_CONNECTION_REQUEST_ACCEPTED;
+	inbox.enqueue(std::move(responseBuffer));
 
-		inbox.enqueue(std::move(buffer));
-	}));
+	try
+	{
+		while (true)
+		{
+			Buffer::length_t length;
+			co_await asio::async_read(socket, asio::mutable_buffer(&length, sizeof(length)), asio::use_awaitable);
+		
+			Buffer buffer(length);
+			co_await asio::async_read(socket, asio::mutable_buffer(buffer.data(), buffer.length()), asio::use_awaitable);
+
+			inbox.enqueue(std::move(buffer));
+		}
+	}
+	catch (const std::system_error& e)
+	{
+		LOG_DEBUG(L"Read failed! %S", e.what());
+		// TODO: Figure out how to tell the client about the disconnect.
+	}
 }
 
 void TCPSocket::disconnect()
 {
-	socket.close(); // TODO: This forcefully disconnects the server, figure out how to do this nicely.
-}
-
-void TCPSocket::doRead()
-{
-	asio::async_read(socket, asio::mutable_buffer(&pendingRead.length, sizeof(pendingRead.length)),
-		asio::bind_executor(strand, [this](asio::error_code error, size_t)
+	asio::post(strand, [this]()
 	{
-		if (error)
-		{
-			LOG_DEBUG(L"Read failed! %S", error.message().c_str());
-			// TODO: Figure out how to tell the client about the disconnect.
-			return;
-		}
-
-		pendingRead.data = std::make_unique<char[]>(pendingRead.length);
-
-		asio::async_read(socket, asio::mutable_buffer(pendingRead.data.get(), pendingRead.length),
-			asio::bind_executor(strand, [this](asio::error_code error, size_t)
-		{
-			if (error)
-			{
-				LOG_DEBUG(L"Read failed! %S", error.message().c_str());
-				// TODO: Figure out how to tell the client about the disconnect.
-				return;
-			}
-
-			inbox.enqueue(std::move(pendingRead));
-
-			doRead();
-		}));
-	}));
+		socket.shutdown(asio::socket_base::shutdown_both);
+		socket.close();
+	});
 }
 
 void TCPSocket::send(const char* data, uint32_t length)
 {
 	Buffer buffer(length);
-	memcpy(buffer.data.get(), data, length);
-
+	memcpy(buffer.data(), data, length);
 	outbox.enqueue(std::move(buffer));
 
-	doCheckWrite();
-}
-
-void TCPSocket::doCheckWrite()
-{
 	asio::post(strand, [this]()
 	{
 		if (!writing)
 		{
 			writing = true;
-			doWrite();
+			asio::co_spawn(strand, writeAsync(), asio::detached);
 		}
 	});
 }
 
-void TCPSocket::doWrite()
+asio::awaitable<void> TCPSocket::writeAsync()
 {
-	if (!outbox.try_dequeue(pendingWrite))
+	try
 	{
-		writing = false;
-		return;
+		while (true)
+		{
+			Buffer buffer;
+			if (!outbox.try_dequeue(buffer))
+				break;
+
+			Buffer::length_t length = buffer.length();
+			co_await asio::async_write(socket, asio::const_buffer(&length, sizeof(length)), asio::use_awaitable);
+			co_await asio::async_write(socket, asio::const_buffer(buffer.data(), buffer.length()), asio::use_awaitable);
+		}
+	}
+	catch (const std::system_error& e)
+	{
+		LOG_DEBUG(L"Write failed! %S", e.what());
 	}
 
-	asio::async_write(socket, asio::const_buffer(&pendingWrite.length, sizeof(pendingWrite.length)),
-		asio::bind_executor(strand, [this](asio::error_code error, size_t) { 
-
-		if (error)
-		{
-			LOG_DEBUG(L"Write failed! %S", error.message().c_str());
-			// TODO: Figure out how to tell the client about the disconnect.
-		}
-
-		asio::async_write(socket, asio::const_buffer(pendingWrite.data.get(), pendingWrite.length),
-			asio::bind_executor(strand, [this](asio::error_code error, size_t)
-		{
-			if (error)
-			{
-				LOG_DEBUG(L"Write failed! %S", error.message().c_str());
-				// TODO: Figure out how to tell the client about the disconnect.
-			}
-
-			pendingWrite.data.release();
-
-			doWrite();
-		}));
-
-	}));
+	writing = false;
 }
 
 char* TCPSocket::receive(uint32_t* length)
@@ -124,11 +103,11 @@ char* TCPSocket::receive(uint32_t* length)
 	if (!inbox.try_dequeue(pendingReceive))
 		return nullptr;
 
-	*length = pendingReceive.length;
-	return pendingReceive.data.get();
+	*length = pendingReceive.length();
+	return pendingReceive.data();
 }
 
 void TCPSocket::receiveAck()
 {
-	pendingReceive.data.release();
+	pendingReceive = Buffer();
 }
--- TreeHouse.Othertransport/src/TCPSocket.h
+++ TreeHouse.Othertransport/src/TCPSocket.h
@@ -10,17 +10,32 @@
 class TCPSocket : public AsyncSocket
 {
 private:
-	struct Buffer
+	class Buffer
 	{
-		uint32_t length;
-		std::unique_ptr<char[]> data;
+	public:
+		using length_t = uint32_t;
 
+	private:
+		length_t _length = 0;
+		std::unique_ptr<char[]> _data;
+
+	public:
 		Buffer() = default;
 
-		explicit Buffer(uint32_t lenght) : length(lenght), data(std::make_unique<char[]>(lenght)) { }
+		explicit Buffer(length_t lenght) : _length(lenght), _data(std::make_unique<char[]>(lenght)) { }
+
+		length_t length() const { return _length; }
+
+		char* data() { return _data.get(); }
+		
+		const char* data() const { return _data.get(); }
+
+		asio::mutable_buffer mutable_buffer() { return asio::mutable_buffer(_data.get(), _length); }
+
+		asio::const_buffer const_bugger() const { return asio::const_buffer(_data.get(), _length); }
 	};
 
-	asio::io_context::strand strand;
+	asio::strand<asio::io_context::executor_type> strand;
 	asio::ip::tcp::socket socket;
 
 	moodycamel::ReaderWriterQueue<Buffer> inbox;
@@ -28,19 +43,14 @@ private:
 
 	Buffer pendingReceive;
 
-	Buffer pendingRead;
-	Buffer pendingWrite;
-
 	bool writing = false;
 
-	void doRead();
-
-	void doCheckWrite();
-	void doWrite();
+	asio::awaitable<void> connectAsync(uint32_t ip, uint16_t port);
+	asio::awaitable<void> writeAsync();
 
 public:
 	explicit TCPSocket(asio::io_context& context)
-		: strand(context), socket(context) { };
+		: strand(context.get_executor()), socket(context) { };
 
 	virtual void connect(uint32_t ip, uint16_t port) override;
 
